3212702e396857788aaf4565a24f3374374595eb
Stardurost <88589361+Stardurost@users.noreply.github.com>
2023-03-28 07:37:30 +0300

    Update CalcController.java

--- practice/src/main/java/ru/neoflex/practice/controller/CalcController.java
+++ practice/src/main/java/ru/neoflex/practice/controller/CalcController.java
@@ -8,21 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalcController {
     @GetMapping("/plus/{a}/{b}")
     public Integer Sum(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {
-        return a + b;                                                                           // вычисление суммы чисел
+        return a + b;                                                                      
     }
 
     @GetMapping("/minus/{a}/{b}")
     public Integer Minus(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {  
-        return a - b;                                                                           // вычисление разности чисел
+        return a - b;                                                                           
     }
-    @GetMapping("/vacation/{CountDaysWork}/{SalaryInYear}/{CountDaysVacation}")
-    public String VacationPayment(@PathVariable("CountDaysWork") Integer countDaysW,            // количесто отработанных дней за год
-                                   @PathVariable("SalaryInYear") Double salaryInYear,           // количествно зарплаты за год
-                                   @PathVariable("CountDaysVacation") Integer countDaysV ) {    // количество дней отпуска для расчёта
-        Double avgSalaryInDay = salaryInYear / countDaysW;      // Средний дневной заработок для оплаты отпускных — это доход за расчетный период, поделенный на количество отработанных дней.
-        Double result = avgSalaryInDay * countDaysV;            // Отпускные = среднедневной заработок × количество дней отпуска
-        String resultString = String.format("%.2f",result);     // округление результатов вычисления
-        return "За отпуск "+countDaysV+" дней сотрудник получит "+resultString+" руб.";         // возвращение результата вычисления 
+    
+    @GetMapping("/vacation/{countDaysWork}/{salaryInYear}/{countDaysVacation}")
+    public String VacationPayment(@PathVariable("countDaysWork") Integer countDaysW,            
+                                   @PathVariable("salaryInYear") Double salaryInYear,           
+                                   @PathVariable("countDaysVacation") Integer countDaysV ) {    
+        Double avgSalaryInDay = salaryInYear / countDaysW;      
+        Double result = avgSalaryInDay * countDaysV;         
+        String resultString = String.format("%.2f",result);    
+        return "За отпуск "+countDaysV+" дней сотрудник получит "+resultString+" руб.";      
     }
 
 }
