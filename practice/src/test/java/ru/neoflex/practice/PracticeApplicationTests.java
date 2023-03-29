67cf93e2da796e8be2bb54fc6dfaa37c521e1f78
Stardurost <stardurost@gmail.com>
2023-03-29 15:32:25 +0300

    Добавление функций тестирования запросов

--- practice/src/test/java/ru/neoflex/practice/PracticeApplicationTests.java
+++ practice/src/test/java/ru/neoflex/practice/PracticeApplicationTests.java
@@ -1,13 +1,69 @@
 package ru.neoflex.practice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class PracticeApplicationTests {
+@AutoConfigureMockMvc
+class PracticeApplicationTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+	public Integer res;	// переменная для вычисления результата запроса
+
+	/**
+	 * Функция тестирования get запроса сложения
+	 * тестовые значения a = 5, b = 2
+	 * 1. Проверка статуса выполнения запроса
+	 * 2. Проверка результата выполнения: результат запроса содержит 7
+	 * @throws Exception
+	 */
 	@Test
-	void contextLoads() {
+	public void shouldReturnSum() throws Exception {
+		res = 5 + 2;
+		this.mockMvc
+			.perform(get("/plus/5/2")) // perform a request that can be chained
+			.andExpect(status().isOk())
+		    .andExpect(result -> result.getRequest().toString().contains(res+"") );
 	}
 
+	/**
+	 * Функция тестирования get запроса разности
+	 * тестовые значения a = 5, b = 2
+	 * 1. Проверка статуса выполнения запроса
+	 * 2. Проверка результата выполнения: результат запроса содержит 3
+	 * @throws Exception
+	 */
+	@Test
+	public void shouldReturnMinus() throws Exception {
+		res = 5 - 2;
+		this.mockMvc
+			.perform(get("/minus/5/2")) // perform a request that can be chained
+			.andExpect(status().isOk())
+			.andExpect(result -> result.getRequest().toString().contains(res+""));
+	}
+	/**
+	 * Функция тестирования get запроса вычисления количества отпускных
+	 * тестовые значения: countDaysWork = 321, salaryInYear = 350000, countDaysVacation = 20
+	 * 1. Проверка статуса выполнения запроса
+	 * 2. Проверка результата выполнения: результат запроса содержит 21806,85
+	 * @throws Exception
+	 */
+	@Test
+	public void shouldReturnVacation() throws Exception {
+		Double resD = 350000 / 321.0 * 20;
+		String resStr = String.format("%.2f",resD);
+		this.mockMvc
+			.perform(get("/vacation/321/350000/20"))
+			.andExpect(status().isOk())
+			.andExpect(result -> result.getRequest().toString().contains(resStr));
+	}
 }
+
+
+
